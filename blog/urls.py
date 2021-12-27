0b61541eccc60179a30d3fc0a85330d8e54c8ce9
on-stu <minsu0523@naver.com>
2021-12-27 21:52:04 +0900

    complete first chapter

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from re import template
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'blog'
+
+urlpatterns = [
+  path('', TemplateView.as_view(template_name="templates/blog/index.html"))
+]
